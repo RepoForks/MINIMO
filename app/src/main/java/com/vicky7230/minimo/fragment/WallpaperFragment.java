0f7c968b8aa90a9325a7ad089569de3572a60ea2
thegod <thegod@thegod.local>
2017-04-02 22:06:32 +0530

    hmmm

--- app/src/main/java/com/vicky7230/minimo/fragment/WallpaperFragment.java
+++ app/src/main/java/com/vicky7230/minimo/fragment/WallpaperFragment.java
@@ -1,30 +1,52 @@
 package com.vicky7230.minimo.fragment;
 
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.vicky7230.minimo.R;
 import com.vicky7230.minimo.adapter.RecyclerViewAdapter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WallpaperFragment extends DialogFragment {
 
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 678;
     private ImageView imageView;
 
+    private Button downloadButton;
+
+    private Bitmap bitmap;
 
     public WallpaperFragment() {
         // Required empty public constructor
@@ -74,14 +96,109 @@ public class WallpaperFragment extends DialogFragment {
             Glide.
                     with(this)
                     .load(bundle.getString(RecyclerViewAdapter.IMAGE_URL))
-                    .crossFade()
-                    .into(imageView);
+                    .asBitmap()
+                    .into(new BitmapImageViewTarget(imageView) {
+                        @Override
+                        protected void setResource(Bitmap resource) {
+                            super.setResource(resource);
+                            bitmap = resource;
+                        }
 
-            //Toast.makeText(getActivity(), "Loading......", Toast.LENGTH_SHORT).show();
+                        @Override
+                        public void onLoadFailed(Exception e, Drawable errorDrawable) {
+
+                        }
+                    });
+
 
         }
 
+        downloadButton = (Button) getView().findViewById(R.id.download_button);
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                if (ContextCompat.checkSelfPermission(getActivity(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+                } else {
+
+                    saveInGallery();
+
+                }
+
+            }
+        });
+
+
+    }
+
+    private void saveInGallery() {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        String folderPath = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES) + "/WallPic";
+        File folder = new File(folderPath);
+        if (!folder.exists()) {
+            File wallpaperDirectory = new File(folderPath);
+            wallpaperDirectory.mkdirs();
+        }
+        File image = null;
+        try {
+            image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".png",         /* suffix */
+                    folder      /* directory */
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String mCurrentPhotoPath = image.getAbsolutePath();
+
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(image);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, out); // Compress Image
+        try {
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        getActivity().sendBroadcast(mediaScanIntent);
+
+
+        Toast.makeText(getActivity(), "Saved in Gallery.", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    saveInGallery();
+                } else {
+                    Toast.makeText(getActivity(), "The app does not have the permission to download the wallpaper.", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+        }
     }
 
 }
